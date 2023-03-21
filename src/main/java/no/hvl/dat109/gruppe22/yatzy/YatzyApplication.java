1a2109bd2954c0868412b7b4555424439e0a7635
h110541 <110541@stud.hvl.no>
2023-03-21 13:59:18 +0100

    Endret indentering fra tabs til spaces

--- src/main/java/no/hvl/dat109/gruppe22/yatzy/YatzyApplication.java
+++ src/main/java/no/hvl/dat109/gruppe22/yatzy/YatzyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YatzyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YatzyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YatzyApplication.class, args);
+    }
 
 }
