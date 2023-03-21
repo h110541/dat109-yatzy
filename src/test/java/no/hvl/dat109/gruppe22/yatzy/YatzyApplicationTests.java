fe83ad9ec62864be18e58f6158602f7dd59769eb
h110541 <110541@stud.hvl.no>
2023-03-21 13:33:37 +0100

    Opprinnelig commit (ingen endringer gjort)

---
+++ src/test/java/no/hvl/dat109/gruppe22/yatzy/YatzyApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.gruppe22.yatzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
